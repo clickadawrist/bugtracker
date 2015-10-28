6fa1eec4dc3be55acaf94d04baece1859bf4800c
magreen3 <magreen3@ncsu.edu>
2015-10-28 19:34:36 -0400

    99% finished with Command class

--- Project2/src/edu/ncsu/csc216/bug_tracker/command/Command.java
+++ Project2/src/edu/ncsu/csc216/bug_tracker/command/Command.java
@@ -1,47 +1,83 @@
 package edu.ncsu.csc216.bug_tracker.command;
 
+import java.util.ArrayList;
+
 /**
  * @author Manaka Green and Paul Hawkins
  *
  */
 public class Command {
 	
-	public static final String R_FIXED;
-	public static final String R_DUPLICATE;
-	public static final String R_WONTFIX;
-	public static final String R_WORKSFORME;
+	public static final String R_FIXED = Resolution.FIXED.toString();
+	public static final String R_DUPLICATE = Resolution.DUPLICATE.toString();
+	public static final String R_WONTFIX = Resolution.WONTFIX.toString();
+	public static final String R_WORKSFORME = Resolution.WORKSFORME.toString();
 	private String developerId;
 	private String note;
 	
-	public Command(CommandValue a, String b, Resolution c, String d) {
-		
+	private CommandValue c;
+	private Resolution resolution;
+	
+	/**
+	 * 
+	 * @param c
+	 * @param developerId
+	 * @param resolution
+	 * @param note
+	 */
+	public Command(CommandValue c, String developerId, Resolution resolution, String note) {
+		this.c = c;
+		this.developerId = developerId;
+		this.note = note;
+		this.resolution = resolution;
 	}
 	
 	/**
-	 * Nested CommandValue Enumeration
+	 * Enumeration.
+	 * Has fixed commands.
 	 */
 	public static enum CommandValue { 
 		VOTE, POSSESSION, RESOLVED, VERIFIED, REOPEN, CONFIRM	
 	}
 	
+	/**
+	 * Enumeration.
+	 * Has fixed resolution outcomes.
+	 *
+	 */
 	public static enum Resolution { 
 		FIXED, DUPLICATE, WONTFIX, WORKSFORME 
 	}
 		
-	
+	/**
+	 * 
+	 * @return command value
+	 */
 	public CommandValue getCommand() {
-		
+		return this.c; 
 	}
 	
+	/**
+	 * 
+	 * @return developer's id
+	 */
 	public String getDeveloperId() {
-		
+		return this.developerId;
 	}
 	
+	/**
+	 * 
+	 * @return resolution
+	 */
 	public Resolution getResolution() {
-		
+		return this.resolution;
 	}
 	
+	/**
+	 * 
+	 * @return note for the bug
+	 */
 	public String getNote() {
-		
+		return this.note;
 	}
 }
