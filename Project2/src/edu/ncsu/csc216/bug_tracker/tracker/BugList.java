729e07da34c3dee6e72c8a88836a0881b093ab41
magreen3 <magreen3@ncsu.edu>
2015-11-01 22:16:41 -0500

    Javadocked BugList class and adjusted spacing.

--- Project2/src/edu/ncsu/csc216/bug_tracker/tracker/BugList.java
+++ Project2/src/edu/ncsu/csc216/bug_tracker/tracker/BugList.java
@@ -6,25 +6,31 @@ import java.util.List;
 import edu.ncsu.csc216.bug_tracker.bug.TrackedBug;
 import edu.ncsu.csc216.bug_tracker.command.Command;
 import edu.ncsu.csc216.bug_tracker.xml.Bug;
-import edu.ncsu.csc216.bug_tracker.xml.BugIOException;
-import edu.ncsu.csc216.bug_tracker.xml.BugReader;
 
 /**
- * @author tmanthawk
- *
+ * Handles lists of bugs between Bug Tracker's XML list, scanned in XML list,
+ * and user generated inputs bug lists.
+ * @author Paul Hawkins and Manaka Green
  */
 public class BugList {
 
+	/** List of tracked bugs.  */
 	private ArrayList<TrackedBug> list;
+	
 	/**
-	 * @throws BugIOException 
-	 * 
+	 * Constructs a new bug list and sets first bug count to 0.
 	 */
 	public BugList()  
 	{
 		TrackedBug.setCounter(0);
 	}
 	
+	/**
+	 * Adds bug to Bug Tracker's XML list of bugs from user input.
+	 * @param summary Description of the bug being added
+	 * @param reporter Name of the person adding the bug
+	 * @return temp List of the newly added bugs
+	 */
 	public int addBug(String summary, String reporter)
 	{
 		TrackedBug temp = new TrackedBug(summary, reporter);
@@ -32,6 +38,11 @@ public class BugList {
 		return temp.getBugId();
 	}
 	
+	/**
+	 * Adds bug to Bug Tracker's XML list of bugs from a different XML.
+	 * Sets id of newly added bugs.
+	 * @param bugList List of bugs from non Bug-Tracker XML
+	 */
 	public void addXMLBug(List<Bug> bugList) 
 	{
 		int maxId = 0;
@@ -44,11 +55,20 @@ public class BugList {
 		TrackedBug.setCounter(maxId + 1);
 	}
 	
+	/**
+	 * Gets bugs from Bug Tracker's list.
+	 * @return list List of bugs from Bug Tracker
+	 */
 	public List<TrackedBug> getBugs()
 	{
 		return list;
 	}
 	
+	/**
+	 * Gets bugs from Bug Tracker's list according to owner.
+	 * @param owner Name of person who's tracking the bug
+	 * @return temp List of bugs from Bug Tracker by owner 
+	 */
 	public List<TrackedBug> getBugsByOwner(String owner)
 	{
 		List<TrackedBug> temp = new ArrayList<TrackedBug>();
@@ -58,11 +78,15 @@ public class BugList {
 			{
 				temp.add(list.get(i));
 			}
-		}
-		
+		}		
 		return temp;
 	}
 	
+	/**
+	 * Gets bugs from Bug Tracker's list according bug's id.
+	 * @param id Integer value of bug's id
+	 * @return list List of bugs from Bug Tracker by id
+	 */
 	public TrackedBug getBugByid(int id)
 	{
 		for(int i = 0; i < list.size(); i++)
@@ -72,15 +96,23 @@ public class BugList {
 				return list.get(i);
 			}
 		}
-		
 		return null;
 	}
 	
+	/**
+	 * Updates the state bug with its unique id.
+	 * @param id Integer value of bug's id
+	 * @param c Command that updates state of bug
+	 */
 	public void excecuteCommand(int id, Command c)
 	{
 		getBugByid(id).update(c);
 	}
 	
+	/**
+	 * Deletes bugs from Bug Tracker's list according to bug's id.
+	 * @param id Integer value of bug's id
+	 */
 	public void deleteBugByid(int id)
 	{
 		for(int i = 0; i < list.size(); i++)
@@ -91,5 +123,4 @@ public class BugList {
 			}
 		}
 	}
-
 }
